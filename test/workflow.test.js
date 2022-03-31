f90a274b31ddb3dc90f0030cd1b79d9925a8faad
gyetvanakos <gyetvanakos@gmail.com>
2022-03-31 12:58:15 +0200

    im stupid

--- test/workflow.test.js
+++ test/workflow.test.js
@@ -2,13 +2,19 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
 const server = require('../server');
-chai.use(chaitHttp);
+chai.use(chaiHttp);
 
 describe('user workflow tests', () => {
     it('should register + login a user, create..', (done) => {
+
+        done();
     });
-    it('should register + login a user, create..', (done) => {
+    it('should be the 2nd task..', (done) => {
+
+        done();
     });
-    it('should register + login a user, create..', (done) => {
+    it('should be the 3rd task', (done) => {
+
+        done();
     });
 });
