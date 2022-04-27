8ad743df84f68fddb4d07d55aed210b3fc8551b7
gyetvanakos <gyetvanakos@gmail.com>
2022-04-27 21:20:31 +0200

    update

--- test/workflow.test.js
+++ test/workflow.test.js
@@ -4,17 +4,41 @@ const chaiHttp = require('chai-http');
 const server = require('../server');
 chai.use(chaiHttp);
 
-describe('user workflow tests', () => {
-    it('should register + login a user, create..', (done) => {
+describe('/First test collection', function (){
+    it('test default API welcome route...', (done) => {
+        chai.request(server)
+        .get('/api/welcome')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            const actualVal = res.body.message;
+            expect(actualVal).to.be.equal('Welcome');
+            done();
+        })     
+    })
 
-        done();
-    });
-    it('should be the 2nd task..', (done) => {
+    it('should post a car', (done) => {
+        let car = {
+                brand: "Test car",
+                model: "Swift 2004",
+                price: 2500,
+                color: "white"  
+        }
 
-        done();
+        chai.request(server)
+        .post('/api/cars')
+        .send(car)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        }) 
     });
-    it('should be the 3rd task', (done) => {
 
-        done();
-    });
-});
+    it('', function (){
+        let expectedVal = 10;
+        let actualVal = 10;
+
+        expect(actualVal).to.be.equal(expectedVal);
+
+    })
+})
